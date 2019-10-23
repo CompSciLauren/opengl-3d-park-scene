8edffcdfde9759ad97c6636b28edcf083dd74500
Lauren Stephenson <compscilauren@gmail.com>
2019-10-22 20:41:15 -0500

    Implement extended controller

--- project2/ExtendedController.cpp
+++ project2/ExtendedController.cpp
@@ -10,12 +10,36 @@ ExtendedController::ExtendedController(const std::string& name, int rcFlags):
 
 void ExtendedController::handleMouseMotion(int x, int y)
 {
-	std::cout << "Implement ExtendedController::handleMouseMotion!\n";
-	// 1. Get a pixel (dx, dy)
-	// 2. update Controller::screenBaseX, Controller::screenBaseY
-	// 3. if translation, map (dx, dy) to (dxInLDS, dyInLDS) and pass to
-	//    ModelView::addToGlobalPan
-	// 4. else if rotation, map (dx, dy) to rotation angles and pass to
-	//    ModelView::addToGlobalRotationDegrees.
-	// 5. do a redraw()
+	// get a pixel (dx, dy)
+	float dx = x - screenBaseX;
+	float dy = y - screenBaseY;
+
+	// update Controller::screenBaseX, Controller::screenBaseY
+	screenBaseX = x;
+	screenBaseY = y;
+	
+	// if translation, map (dx, dy) to (dxInLDS, dyInLDS) and pass to
+	// ModelView::addToGlobalPan
+	if (mouseMotionIsTranslate)
+	{
+		double ldsX, ldsY;
+		pixelPointToLDSPoint(x, y, ldsX, ldsY);
+		float dxInLDS = x;
+		float dyInLDS = y;
+		float dzInLDS = 0;
+		ModelView::addToGlobalPan(dxInLDS, dyInLDS, dzInLDS);
+	}
+	
+	// else if rotation, map (dx, dy) to rotation angles and pass to
+	// ModelView::addToGlobalRotationDegrees.
+	else if (mouseMotionIsRotate)
+	{
+		float rx = dx;
+		float ry = dy;
+		float rz = 0;
+		ModelView::addToGlobalRotationDegrees(rx, ry, rz);
+	}
+	
+	// do a redraw()
+	redraw();
 }
